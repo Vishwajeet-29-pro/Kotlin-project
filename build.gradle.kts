807b2ca0923f2fbb6ce9b0b86be3447fa13cb3c7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-29 20:11:32 +0530

    build: Add dependencies for Junit 5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
 }
 
 tasks.test {
