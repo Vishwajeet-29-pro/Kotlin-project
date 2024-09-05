7a4c6ccec030fafe2417196cc91ffa2114bf3537
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-05 16:20:41 +0530

    feat: Add code and notes for early return function.

--- src/main/kotlin/com/practice/examples/function/Functions2.kt
+++ src/main/kotlin/com/practice/examples/function/Functions2.kt
@@ -17,3 +17,27 @@ fun add(i: Int, j: Int): Int {
 // also omit the return type. The add() function then becomes one line:
 // for conflict change name of the function.
 fun sum(i: Int, j: Int) = i + j
+
+// Early returns in functions
+// to stop the code in your function being processed further than a certain point, use the return
+// keyword. This example uses if to return from a function earl if the conditional expression is found to be true.
+// A list of registered usernames
+val registeredUsername = mutableListOf("john_doe", "john_smith","john_snow")
+// A list of registered emails
+val registeredEmails = mutableListOf("johndoe@example.com", "johnsmith@example.com","johnsnow@example.com")
+
+fun registerUser(username: String, email: String): String {
+    // Early return if the username is already taken
+    if (username in registeredUsername) {
+        return "Username already taken, Please choose a different one."
+    }
+    // Early return if the email is already registered
+    if (email in registeredEmails) {
+        return "Email already registered. Please use a different the email."
+    }
+    // proceed with the registration if the username and email are not taken
+    registeredEmails.add(username)
+    registeredEmails.add(email)
+
+    return "User registered successfully: $username"
+}
