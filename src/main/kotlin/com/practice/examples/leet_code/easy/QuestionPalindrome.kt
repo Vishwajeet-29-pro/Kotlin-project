625a84dd96ac94ab881f368b11f3cacf3782f784
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-19 13:07:37 +0530

    fix: Update the logic to make all test pass in leetcode

--- src/main/kotlin/com/practice/examples/leet_code/easy/QuestionPalindrome.kt
+++ src/main/kotlin/com/practice/examples/leet_code/easy/QuestionPalindrome.kt
@@ -7,10 +7,11 @@ class QuestionPalindrome {
         if (x < 0) return false
 
         var reverse = 0
-        while ( x > reverse) {
+        val copy = x
+        while ( x > 0) {
             reverse = reverse * 10 + x % 10
             x /= 10
         }
-        return reverse == x || reverse / 10 == x
+        return reverse == copy
     }
 }
