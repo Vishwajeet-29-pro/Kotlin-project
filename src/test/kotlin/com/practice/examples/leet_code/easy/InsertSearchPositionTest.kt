2e30b28114e5133ac0913dcfc3650cb768c67cab
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-09-22 17:36:59 +0530

    test: Add tests for checking edge cases and update array element.

--- src/test/kotlin/com/practice/examples/leet_code/easy/InsertSearchPositionTest.kt
+++ src/test/kotlin/com/practice/examples/leet_code/easy/InsertSearchPositionTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
 
 class InsertSearchPositionTest {
     private val insertSearchPosition: QuestionInsertSearchPosition = QuestionInsertSearchPosition()
-    private var arr: IntArray = intArrayOf(1, 3, 5, 7)
+    private var arr: IntArray = intArrayOf(1, 3, 5, 6)
 
     @Test
     fun `test insert search position algorithm` () {
@@ -13,5 +13,19 @@ class InsertSearchPositionTest {
         val expected = 2
         val actual = insertSearchPosition.searchInsert(arr, target)
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test insert search position for smaller number not present in array` () {
+        val target = 2
+        val expected = 1
+        assertEquals(expected, insertSearchPosition.searchInsert(arr, target))
+    }
+
+    @Test
+    fun `test insert search position for bigger number not present in array` () {
+        val target = 7
+        val expected = 4
+        assertEquals(expected, insertSearchPosition.searchInsert(arr, target))
     }
 }
