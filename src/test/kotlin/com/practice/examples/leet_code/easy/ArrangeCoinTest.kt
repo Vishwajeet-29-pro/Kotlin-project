b333051a25ba73baa9f8e1997e16b0cd84e61874
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-25 11:23:54 +0530

    test: Add more tests along with edge cases like integer overflow

--- src/test/kotlin/com/practice/examples/leet_code/easy/ArrangeCoinTest.kt
+++ src/test/kotlin/com/practice/examples/leet_code/easy/ArrangeCoinTest.kt
@@ -13,4 +13,16 @@ class ArrangeCoinTest {
         val actual = arrangeCoins.arrangeCoins(input)
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun `test arrange coins for input 8`() {
+        assertEquals(3, arrangeCoins.arrangeCoins(8))
+    }
+
+    @Test
+    fun `test integer overflow for arrange coins` () {
+        val input = 1804289383
+        val expected = 60070
+        assertEquals(expected, arrangeCoins.arrangeCoins(input))
+    }
 }
