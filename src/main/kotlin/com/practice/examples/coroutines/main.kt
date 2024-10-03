b1298ab43abb7779997ea38139651797b64ea7eb
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-03 07:57:14 +0530

    feat: Test context switching by calling method in main.

--- src/main/kotlin/com/practice/examples/coroutines/main.kt
+++ src/main/kotlin/com/practice/examples/coroutines/main.kt
@@ -3,9 +3,12 @@ package com.practice.examples.coroutines
 // common method to call other thread method.
 fun main() {
 
-    mainLaunchSimple() // launch simple method
-    println()
-    mainCoroutine() // coroutine example
-    println()
-    mainDispatcher() // Dispatcher and Threading example
+//    mainLaunchSimple() // launch simple method
+//    println()
+//    mainCoroutine() // coroutine example
+//    println()
+//    mainDispatcher() // Dispatcher and Threading example
+
+    println("Using withContext for Context Switching...")
+    mainWithContext() // for context Switching
 }
