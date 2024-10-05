402f1e702185c484160d7dbed659c3c628b5162b
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-05 16:48:36 +0530

    test: Add tests for strings are not equal and multiple array element with single element.

--- src/test/kotlin/com/practice/examples/leet_code/easy/string/ArrayStringEqualTest.kt
+++ src/test/kotlin/com/practice/examples/leet_code/easy/string/ArrayStringEqualTest.kt
@@ -1,6 +1,7 @@
 package com.practice.examples.leet_code.easy.string
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class ArrayStringEqualTest {
@@ -11,6 +12,22 @@ class ArrayStringEqualTest {
     fun `test array strings are equal` () {
         val word1: Array<String> = arrayOf("a", "bc")
         val word2: Array<String> = arrayOf("ab", "c")
+
+        assertTrue(stringsEqual.arrayStringsAreEqual(word1, word2))
+    }
+
+    @Test
+    fun `test array strings are not equal` () {
+        val word1: Array<String> = arrayOf("a", "bc")
+        val word2: Array<String> = arrayOf("a", "cb")
+
+        assertFalse(stringsEqual.arrayStringsAreEqual(word1, word2))
+    }
+
+    @Test
+    fun `test array string are equal if second input has single word`() {
+        val word1: Array<String> = arrayOf("a", "bc", "def", "g")
+        val word2: Array<String> = arrayOf("abcdefg")
 
         assertTrue(stringsEqual.arrayStringsAreEqual(word1, word2))
     }
