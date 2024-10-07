9ffaedda375f5b88d27fed66b0ed2174b2f77870
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-07 22:40:06 +0530

    feat: Implement logic in merge alternatively method and make test pass.

--- src/main/kotlin/com/practice/examples/leet_code/easy/string/QuestionMergeStringsAlternatively.kt
+++ src/main/kotlin/com/practice/examples/leet_code/easy/string/QuestionMergeStringsAlternatively.kt
@@ -1,9 +1,17 @@
 package com.practice.examples.leet_code.easy.string
 
+import kotlin.math.max
+
 class QuestionMergeStringsAlternatively {
 
     fun mergeAlternately(word1: String, word2: String): String {
+        val builder = StringBuilder()
+        val wordLength = max(word1.length, word2.length)
 
-        return ""
+        for (i in 0..wordLength) {
+            if (i < word1.length) builder.append(word1[i])
+            if (i < word2.length) builder.append(word2[i])
+        }
+        return builder.toString()
     }
 }
