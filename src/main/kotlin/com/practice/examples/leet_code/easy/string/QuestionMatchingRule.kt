2d4129cdbcce2aa407e4eeeeb271748268d20af5
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-08 19:20:00 +0530

    feat: Add logic into countMatches function and make test pass.

--- src/main/kotlin/com/practice/examples/leet_code/easy/string/QuestionMatchingRule.kt
+++ src/main/kotlin/com/practice/examples/leet_code/easy/string/QuestionMatchingRule.kt
@@ -3,6 +3,17 @@ package com.practice.examples.leet_code.easy.string
 class QuestionMatchingRule {
     fun countMatches(items: List<List<String>>, ruleKey: String, ruleValue: String): Int {
 
-        return 0
+        var counter = 0
+        var index = 0
+
+        if (ruleKey == "color") index = 1
+        if (ruleKey == "name") index = 2
+
+        for (i in items) {
+            if (i[index] == ruleValue) {
+                counter++
+            }
+        }
+        return counter
     }
 }
